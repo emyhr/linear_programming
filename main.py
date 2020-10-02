1584260b843aa84ac53a127a506483d2e695fbc3
emyhr <enurmatbekoV@gmail.com>
2020-10-02 15:54:32 +0200

    readme added\n correlations added\n

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mrdoe import *
 from pulp import *
+from scipy.stats import kendalltau, spearmanr
 
 
 def main():
@@ -14,19 +15,35 @@ def main():
     problem += lpDot(v_places.values(), DURATION) <= MAX_HOURS, "Time constraint"
     problem += lpDot(v_places.values(), PRICE) <= MAX_EUROS, "Money constraint"
 
-    # List of recommended PLACES
+    # List of recommended places
     problem.solve()
     ListVisit1 = [var.name for var in problem.variables() if var.varValue]
 
     # preferences
-    preferences = [1, 2, 3, 4]
+    preferences = [1, 2, 3, 4, 5]
     preferred_places = set_preferences(problem, preferences, v_places)
+
+    # correlations of rankings
+    ken_dur_app, _ = kendalltau(DURATION, APPRECIATION)
+    spearman_dur_app, _ = spearmanr(DURATION, APPRECIATION)
+    ken_dur_price, _ = kendalltau(DURATION, PRICE)
+    spearman_dur_price, _ = spearmanr(DURATION, PRICE)
+    ken_price_app, _ = kendalltau(PRICE, APPRECIATION)
+    spearman_price_app, _ = spearmanr(PRICE, APPRECIATION)
 
     problem.writeLP("Paris_places.lp")
     print("Status:", LpStatus[problem.status])
-    print("Recommended PLACES:\n", ListVisit1)
-    print("Total PLACES visited = ", value(problem.objective))
-    print(equal_lists(ListVisit1, preferred_places))
+    print("Recommended places:\n", ListVisit1)
+    print("Total places visited = ", value(problem.objective))
+    print(equal_lists(preferred_places, ListVisit1))
+    print(preferred_places)
+
+    print("Kendall correlation between duration and appreciation: ", ken_dur_app)
+    print("Spearman correlation between duration and appreciation: ", spearman_dur_app)
+    print("Kendall correlation between duration and price: ", ken_dur_price)
+    print("Spearman correlation between duration and price: ", spearman_dur_price)
+    print("Kendall correlation between price and appreciation: ", ken_price_app)
+    print("Spearman correlation between price and appreciation: ", spearman_price_app)
 
 
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Decision modelling
+## Assignment 2. Linear programming
+
+1.   `ListVisit1 = ['AC', 'AT', 'BS', 'CA', 'CN', 'PC', 'SC']`
+
+2.  (a) Lists of tourist sites from preferences 1-4 are not the same as `       ListVisit1` but the list from preference 5 is the same as `ListVisit1`.  
+    (b) List from preferences 1 and 2: `['AC', 'AT', 'CA', 'TE', 'TM']`  
+    (c) List from preferences 1 and 3: `['v_AC', 'v_AT', 'v_CA', 'v_JT', 'v_MO', 'v_PC', 'v_TM']`  
+    (d) List from preferences 1 and 4: `['AC', 'AT', 'CA', 'JT', 'MO', 'PC', 'TM']`  
+    (e) List from preferences 2 and 5: `['AC', 'AT', 'CA', 'PC', 'SC', 'TE']`  
+    (f) List from preferences 3 and 4: `['AC', 'AT', 'BS', 'CA', 'PC', 'SC', 'TM']`  
+    (g) List from preferences 4 and 5: `['AC', 'AT', 'BS', 'CN', 'PC', 'SC', 'TM']`  
+    (h) List from preferences 1, 2 and 4: `['AC', 'AT', 'CA', 'TE', 'TM']`  
+    (i) List from preferences 2, 3 and 5: `['v_AC', 'v_AT', 'v_CA', 'v_JT', 'v_PC', 'v_TE']`  
+    (j) List from preferences 2, 3, 4 and 5: `['v_AC', 'v_AT', 'v_CA', 'v_PC', 'v_TE', 'v_TM']` 
+    (k) List from preferences 1, 2, 4 and 5: `['v_AC', 'v_AT', 'v_CA', 'v_TE', 'v_TM']`  
+    (l) List from preferences 1, 2, 3, 4 and 5: `['v_AC', 'v_AT', 'v_CA', 'v_TE', 'v_TM']`  
+
+    (m) None of the combinations above give the same solution as `ListVisit1`
+
+3.  Kendall and Spearman correlations for all pairs of rankings:  
+    Kendall correlation between duration and appreciation:  `0.1515325460564539`   
+    Spearman correlation between duration and appreciation:  `0.2058187176566192`  
+    Kendall correlation between duration and price:  `0.605587290764896`  
+    Spearman correlation between duration and price:  `0.7413325704498833`  
+    Kendall correlation between price and appreciation:  `-0.08521145659838941`  
+    Spearman correlation between price and appreciation:  `-0.13400233411084597`  
